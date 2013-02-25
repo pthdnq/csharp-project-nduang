b87f0296f2209ba2822c1d528df4a7bbb9592a6d
dangquangk53a3 <dangquangk53a3@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-02-25 12:45:50 +0000

    update file

---
+++ trunk/HN36Pho/DataLayer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace HN36Pho
+{
+    class DataLayer
+    {
+        public SqlConnection conn = new SqlConnection();
+        public bool OpenConnection()
+        {
+            conn.ConnectionString=ModuleConfig.strConnection;
+            conn.Open();
+            if (conn==null)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool CloseConnection()
+        {
+            
+            if (conn!=null)
+            {
+                conn.Close();
+                return false;
+            }
+            return true;
+        }
+        public DataTable getData(string sqlQuery)
+        {
+            OpenConnection();
+            SqlDataAdapter ad = new SqlDataAdapter(sqlQuery, conn);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            CloseConnection();
+            return dt;
+        }
+        public bool updateData(string sqlQuery)
+        {
+            OpenConnection();
+            SqlCommand sqlCommand = new SqlCommand(sqlQuery, conn);
+            int Result=sqlCommand.ExecuteNonQuery();
+            CloseConnection();
+            if (Result==-1)
+            {
+                return false;
+            }
+            return true;
+        }
+        public int countRow(string sqlQuery)
+        {
+            OpenConnection();
+            SqlDataAdapter ad = new SqlDataAdapter(sqlQuery, conn);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            int numberRow = dt.Rows.Count;
+            CloseConnection();
+            return numberRow;
+        }
+    }
+}
---
+++ trunk/HN36Pho/Pho.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace HN36Pho
+{
+    class Pho
+    {
+        DataLayer DL =new DataLayer();
+        public bool addPho(string sqlQuery)
+        {
+            return DL.updateData(sqlQuery);
+        }
+        public bool editPho(string sqlQuery)
+        {
+            return DL.updateData(sqlQuery);
+        }
+        public bool delPho(string sqlQuery)
+        {
+            return DL.updateData(sqlQuery);
+        }
+        public DataTable getDataPho(string sqlQuery)
+        {
+            DataTable dt=DL.getData(sqlQuery);
+            return dt;
+        }
+    }
+}
