ba966ec2d494d8b5f7f75b2c9a6b449550116b2d
dangquangk53a3 <dangquangk53a3@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-02-26 14:49:46 +0000

    update Exercises/ToLower

---
+++ trunk/Exercises/ToLower/ToLower/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ToUpper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string chuoiHoa = txtChuHoa.Text;// lay chu Hoa vao bien chuoi
+            string chuoiThuong = chuoiHoa.ToLower();//doi chu hoa sang chu thuong va gan vao chuoiThuong
+            txtChuThuong.Text = chuoiThuong;// gan chuoi hoa len textbox
+        }
+    }
+}
