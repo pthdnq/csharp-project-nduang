a1df0f6e160e86d6ff848209f2a1cfddf712055d
dangquangk53a3 <dangquangk53a3@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-02-26 13:38:56 +0000

    update folder

---
+++ trunk/Exercises/KeyCode/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Enter)
+            {
+                //txtBox1.Text = txtBox2.Text;
+                //txtBox1.Text = "";
+               
+            }
+        }
+
+        private void txtBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtBox2.Text = txtBox1.Text;// gan chu o duoi len o tren
+                txtBox1.Text = "";// xoa chu o duoi
+
+            }
+
+        }
+    }
+}
