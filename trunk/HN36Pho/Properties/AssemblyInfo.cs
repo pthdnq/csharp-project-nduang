ffc93bfe7b7bddf92631938c0c9a074bd6f8e4ee
dangquangk53a3 <dangquangk53a3@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-02-28 11:00:56 +0000

    update sofware

--- trunk/HN36Pho/Properties/AssemblyInfo.cs
+++ trunk/HN36Pho/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("HN36Pho")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("Copyright © Hoc vien mat ma")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
