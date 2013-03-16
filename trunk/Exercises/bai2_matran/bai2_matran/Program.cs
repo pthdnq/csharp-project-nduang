970526b2d16919396d240b9211e98f4c781b29d4
coc.ngoc9099 <coc.ngoc9099@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-03-16 09:31:30 +0000

    ReadFile

--- trunk/Exercises/bai2_matran/bai2_matran/Program.cs
+++ trunk/Exercises/bai2_matran/bai2_matran/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using ConsoleApplication3;
 namespace bai2_matran
 {
     class Program
@@ -11,9 +11,13 @@ namespace bai2_matran
         {
             int err=0;
             xuly_matran xl = new xuly_matran();
-            xl.NhapMT();
+           // xl.NhapMT();
+            int [,] MT;
+            int m,n;
+            Matrantruyxuattufile mtFile = new Matrantruyxuattufile();
+            mtFile.ReadFile("matran.txt", out m,out n, out MT);
             int dem1;
-            err=xl.demsoduong(out dem1);
+            err=xl.demsoduong(out dem1,m,n,MT);
             Console.Write("so duong la : " + dem1);
             Console.ReadLine();
             
