a1414a41ce0dadce4f652029aea334e9e23ce13e
coc.ngoc9099@gmail.com <coc.ngoc9099@gmail.com@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-04-03 15:19:32 +0000

    ke thua

---
+++ trunk/Exercises/dientich_kethua/dientich_kethua/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dientich_kethua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hinhtron htron = new Hinhtron();
+            htron.hienThi();
+            Hinhtru htru = new Hinhtru();
+            htru.htHT();
+        }
+    }
+    class Hinhtron
+    {
+        double r;
+        double p;
+        double dt;
+        public double dtHinhtron(double r)
+        {
+            double p = 3.14;
+            double s = p * r * r;
+            return s;
+        }
+        public void hienThi()
+        {
+            Console.Write("Nhap Ban kinh : ");
+            double val1 = Convert.ToInt32(Console.ReadLine());
+            dt = dtHinhtron(val1);
+            Console.Write("dientich : " + dt);
+            Console.ReadLine();
+        }
+    }
+    class Hinhtru : Hinhtron
+    {
+        private float h;
+        private double thetich;
+        
+        public double dtHinhtru(double r,double h)
+        {
+
+            double ttht = base.dtHinhtron(r) * h;
+            return ttht;
+          
+        }
+        public void htHT()
+        {
+            Console.Write("Nhap Ban kinh : ");
+            double val1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap chieu cao : ");
+            double val2 = Convert.ToInt32(Console.ReadLine());
+            thetich = dtHinhtru(val1, val2);
+            Console.Write("the tich : " + thetich);
+            Console.ReadLine();
+        }
+
+
+        }
+    }
+
