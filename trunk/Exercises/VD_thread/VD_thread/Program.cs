64c53b487645ac5eeb0df3300d4d3d653166b654
bboysky93no1 <bboysky93no1@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-04-07 16:16:23 +0000

    ví dụ về đa luồng

---
+++ trunk/Exercises/VD_thread/VD_thread/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace VD_thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(MethodA));
+            t.Start();
+            MethodB();
+        }
+        static void MethodA()
+        {
+            StreamWriter wr = new StreamWriter("Text.txt");
+            wr.Write("100-200-300");
+            wr.Close();
+            Console.ReadLine();
+            
+        }
+        static void MethodB()
+        {
+            StreamReader doc = new StreamReader("Text.txt");
+            
+            string docline = doc.ReadLine();
+            String[] XYZ = docline.Split('-');
+            int X = int.Parse(XYZ[0]);
+            int Y = int.Parse(XYZ[1]);
+            int Z = int.Parse(XYZ[2]);
+            doc.Close();
+            Console.ReadLine(); 
+
+        }
+    }
+}
