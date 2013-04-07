1b6100607ed5e67d961b48c7dc56c84653e6a1c1
dangquangk53a3 <dangquangk53a3@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-04-07 18:01:35 +0000

    update thread

--- Exercises/tinh tong so chan/ConsoleApplication1/Program.cs
+++ Exercises/tinh tong so chan/ConsoleApplication1/Program.cs
@@ -2,35 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.IO;
+using System.Collections;
 
 namespace ConsoleApplication1
 {
     class Program
     {
-             static void tinhtong()
+        int m_TongChan = 0;
+        int m_TongLe = 0;
+        int[] niz;
+        public void TaoMangSoNgauNhien()
         {
-            Console.Write("Nhap N= ");
-            int n=int.Parse(Console.ReadLine());
-
-            Console.Write("Cac so chan: ");
-
-            int tong = 0;
-            for (int i = 2; i <= n;i++ )
-            {
-                if (i%2==0)
-                {
-                    Console.Write("{0}   ", i); 
-                    tong=tong+i;
+               niz = new int[100000];
+               //int [] niz = new int[100000];
+               for (int i = 0; i < 100; i++) 
+               { 
+                    Random m = new Random();
+                    int ZOMG = m.Next(0,100000);
+                    niz[i] = ZOMG;
+                    Console.Write(niz[i]);//in ra man hinh xem thu
                 }
-
+        }
+        public void tongChan()
+        {
+            int iTongChan=0;
+            for (int i = 0; i < 100000;i++ )
+            {
+                if(niz[i]%2==0)//phan tu thu i trong mang chia het cho 2
+                {
+                    iTongChan = iTongChan + niz[i];//dua gia tri cua phan tu do vao tong chan
+                }
             }
-            Console.WriteLine();
-            Console.WriteLine("Tong cac so chan: {0}", tong);
+            Console.WriteLine("Tong chan la" + iTongChan);
         }
         static void Main(string[] args)
         {
-            tinhtong();
             Console.ReadLine();
         }
-        }
     }
+}
