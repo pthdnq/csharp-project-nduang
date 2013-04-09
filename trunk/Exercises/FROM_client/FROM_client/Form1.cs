b945d8035471ef8e16e31c70aabffc07d8340afd
bboysky93no1 <bboysky93no1@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-04-09 16:31:28 +0000

    a sửa hộ e vd FROM_client :D

---
+++ trunk/Exercises/FROM_client/FROM_client/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FROM_client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ie = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000);
+            s.Connect(ie);
+            MessageBox.Show("Connected to Server.....");
+            byte[] data = new byte[1024];
+            int k = s.Receive(data);
+            string A = txtNumber1.Text;
+            string B = txtNumber2.Text;
+            float Number1 = float.Parse(A);
+            float Number2 = float.Parse(B);
+//             float ketqua = sothu1 + sothu2;
+//             txtKQ.Text = ketqua.ToString();
+           // txtKQ.Text += " = ";
+            data = Encoding.ASCII.GetBytes(txtNumber1.Text);
+            data = Encoding.ASCII.GetBytes(txtNumber2.Text);
+            s.Send(data, data.Length, SocketFlags.None);
+            data = new byte[1024];
+            txtKQ.Text += Encoding.ASCII.GetString(data, 0, k);
+
+        }
+    }
+}
