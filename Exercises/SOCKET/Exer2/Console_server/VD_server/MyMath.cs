cede426837484eb6e4a4d2cf85ee33fc54278c59
dangquangk53a3 <dangquangk53a3@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-04-10 15:30:42 +0000

    correct folder structure

---
+++ Exercises/SOCKET/Exer2/Console_server/VD_server/MyMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VD_server
+{
+    class MyMath
+    {
+        public float TinhTong(float a, float b)
+        {
+            float Tong = a + b;
+            return Tong;
+        }
+        public void Tachchuoi(string strA,out float A,out float B)
+        {
+            string[] cat = strA.Split('-');
+            A = float.Parse(cat[0]);
+            B = float.Parse(cat[1]);
+        }
+    }
+}
