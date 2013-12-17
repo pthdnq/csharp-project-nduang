a0e533b94ddf53ce9809a5ae745e89abb74d32a6
bang200391 <bang200391@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-12-17 14:12:43 +0000

    correct login

--- trunk/Projects/QLPhuongTien/dev/src/QLPT/FrmDangNhap.cs
+++ trunk/Projects/QLPhuongTien/dev/src/QLPT/FrmDangNhap.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-
+using Component;
 
 namespace QLPT
 {
     public partial class FrmDangNhap : Form
     {
+        private Account m_account = new Account();
         public FrmDangNhap()
         {
             InitializeComponent();
@@ -21,62 +22,25 @@ namespace QLPT
         {
             return new SqlConnection(@"Data Source=(local);Initial Catalog=QuanLy_PT;Integrated Security=True");
         }
-        public DataTable checkLog(string user, string pass)
-        {
-            string sql = " select * from DangNhap where UserName = '" + user + "' and Pass = '" + pass + "'";
-            SqlConnection con = getConnect();
-            SqlDataAdapter ad = new SqlDataAdapter(sql, con);
-            DataTable dt = new DataTable();
-            ad.Fill(dt);
-            return dt;
-
-
-        }
         private void FrmDangNhap_Load(object sender, EventArgs e)
         {
             this.txtUser.Focus();
             AcceptButton = btDangNhap;
-            try
-            {
-
-                SqlConnection con = getConnect();
-                this.Status.ForeColor = Color.Blue;
-                this.Status.Text = "Kết nối Database thành công !";
-            }
-            catch
-            {
-                this.Status.ForeColor = Color.Red;
-                this.Status.Text = "Kết nối Database thất bại!";
-            }
         }
 
         private void btDangNhap_Click(object sender, EventArgs e)
         {
-            if (this.txtUser.Text.Length == 0 || this.txtPass.Text.Length == 0)
+            bool ok = validAccount();
+            if (ok == false)
+                return;
+            bool isExistAccount = m_account.isExistAccount(txtUser.Text.Trim(), txtPass.Text.Trim());
+            if (isExistAccount)
             {
-                this.lbStatus.ForeColor = Color.Red;
-                lbStatus.Text = "Bạn chưa nhập User hoặc Pass";
+                this.Hide();
             }
             else
             {
-                DataTable dt = new DataTable();
-                dt = checkLog(this.txtUser.Text, this.txtPass.Text);
-                if (dt.Rows.Count > 0)
-                {
-                    this.Hide();
-                   // MessageBox.Show("Đăng nhập thành công tài khoản : " + txtUser.Text, "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    //  Form1 frm = new Form1();
-                    // FrmQLPT frmQLPT = new FrmQLPT();
-                    //frm.Show();
-                }
-                else
-                {
-                    this.lbStatus.ForeColor = Color.Red;
-                    this.lbStatus.Text = "Tài khoản không tồn tại";
-                    this.txtUser.Text = "";
-                    this.txtPass.Text = "";
-                    this.txtUser.Focus();
-                }
+                resetControl();
             }
         }
 
@@ -88,6 +52,25 @@ namespace QLPT
         internal void ShowDiaLog()
         {
             throw new NotImplementedException();
+        }
+        private bool validAccount()
+        {
+            if (this.txtUser.Text.Length == 0 || this.txtPass.Text.Length == 0)
+            {
+                this.lbStatus.ForeColor = Color.Red;
+                lbStatus.Text = "Bạn chưa nhập User hoặc Pass";
+                return false;
+            }
+
+            return true;
+        }
+        public void resetControl()
+        {
+            this.lbStatus.ForeColor = Color.Red;
+            this.lbStatus.Text = "Tài khoản không tồn tại";
+            this.txtUser.Text = "";
+            this.txtPass.Text = "";
+            this.txtUser.Focus();
         }
     }
 }
