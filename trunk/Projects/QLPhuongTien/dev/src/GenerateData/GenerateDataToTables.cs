16a96912a09716d6192e3c35a9839bf929de4be7
dangquangk53a3 <dangquangk53a3@6b45d66b-1f0b-c7bd-4c15-4b49e01f4517>
2013-12-21 08:08:27 +0000

    generate data for tables

---
+++ trunk/Projects/QLPhuongTien/dev/src/GenerateData/GenerateDataToTables.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAcess;
+using Component;
+namespace GenerateData
+{
+    public class GenerateDataToTables
+    {
+        DonViQuanLyData m_DonViQuanLyData = new DonViQuanLyData();
+        Utils utils = new Utils();
+        public void generateDonViQuanLyData()
+        {
+            string DonViQLID
+            , DonViQLTen
+            , DonViQLToTruong
+            , Sdt
+            , Email;
+            for (int i = 0; i < 100;i++ )
+            {
+                DonViQLID = "DVQL" + utils.convertIntToFormatedString(i);
+                DonViQLTen = "Đơn vị quản lý số " + i.ToString();
+                DonViQLToTruong = "Bằng Còi " + i.ToString();
+                Sdt = (1638543534 + i).ToString();
+                Email = "bangcoi2013_"+i.ToString()+"@"+"gmail.com";
+
+                m_DonViQuanLyData.insert
+                (
+                    DonViQLID
+                  , DonViQLTen
+                  , DonViQLToTruong
+                  , Sdt
+                  , Email
+                );
+            }
+
+        }
+    }
+}
